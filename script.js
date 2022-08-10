f1f74b54cb3ee4a7f3b3b29774803666fe8af97a
Gustavo Mello <gustavomello@outlook.com>
2022-08-09 23:44:52 -0300

    f to create book objects and cards from input

--- script.js
+++ script.js
@@ -5,14 +5,80 @@ function Book(title, author, pages, read){
     this.author = author
     this.pages = pages
     this.read = read
-    this.info = function(){
-        return(title + ' by ' + author +', ' + pages + ' pages, ' + read)
-    }
 }
 
 function addBookToLibrary(title, author, pages, read){
     let a = new Book(title, author, pages, read);
     myLibrary.push(a);
+}
+
+let addBook = document.getElementById("add");
+
+addBook.addEventListener('click', event => {
+   let a = document.getElementById("Title").value;
+   let b = document.getElementById("Author").value;
+   let c = document.getElementById("Pages").value;
+   let d = document.getElementById("Status").checked;
+   addBookToLibrary(a,b,c,d);
+   deleteAllCards();
+   buildLibraryCards();
+});
+
+function deleteAllCards() {
+    let a = document.getElementsByClassName("book");
+    while(a[0]){
+        a[0].parentNode.removeChild(a[0]);
+    }
+
+}
+
+function buildLibraryCards() {
+    myLibrary.forEach(function (bookItem, index){
+        a = bookItem.title;
+        b = bookItem.author;
+        c = bookItem.pages;
+        d = bookItem.read;
+        e = index;
+        buildBookCard(a,b,c,d,e);
+    }
+    
+    );
+
+}
+
+
+function buildBookCard(title, author, pages, read, index){
+    const books = document.querySelector('.books');
+    let nBook = document.createElement("div");
+    nBook.setAttribute('data',e);
+    let nTitle = document.createElement("h3");
+    nTitle.textContent = title;
+    let nAuthor = document.createElement("p");
+    nAuthor.textContent = 'By ' + author;
+    let nPages = document.createElement("p");
+    nPages.textContent = pages + ' pages';
+    if (read == true) {nBook.classList.add("book-read")} else {
+        nBook.classList.add("book-unread")
+    };
+    let iconsdiv = document.createElement("div");
+    iconsdiv.classList.add("book-icons")
+    let icon1 = document.createElement("img");
+    icon1.src="images/book-check-outline.svg";
+    let icon2 = document.createElement("img");
+    icon2.src="images/book-cancel-outline.svg";
+    let icon3 = document.createElement("img");
+    icon3.src="images/book-minus-outline.svg";
+    iconsdiv.appendChild(icon1);
+    iconsdiv.appendChild(icon2);
+    iconsdiv.appendChild(icon3);
+
+
+    nBook.classList.add("book");
+    nBook.appendChild(nTitle);
+    nBook.appendChild(nAuthor);
+    nBook.appendChild(nPages);
+    nBook.appendChild(iconsdiv);
+    books.insertBefore(nBook, books.firstChild);
 }
 
 // const theHobbit = new Book('The Hobbit','J.R.R. Tolkien','295', 'not read yet')
